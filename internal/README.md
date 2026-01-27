6faf1b84a1a27095fa26d87528c5ea7d0b8745a8
Shynggys <s.terekbayev@andersenlab.eu>
2026-01-27 06:00:53 +0500

    left tempates: repo, service, transport, infrastructure

---
+++ internal/README.md
@@ -0,0 +1,6 @@
+# INTERNAL
+The heart of your app. Here you store:
+
+* **Domain**: (Business entities whe are working with, errors related to them)
+* **Feature**: (Business logic)
+* **Infrastructure**: (non-business connection with needed pkgs, technologies)
---
+++ internal/domain/user/user.go
@@ -0,0 +1,19 @@
+package user
+
+// The struct already contains aggregated data from different User models that represent tabel entity
+// No JSON, nor Validation, nor DB tags should be there.
+
+type User struct {
+	// From users table
+	ID    uint64
+	Email string
+	// From user_infos table
+	FirstName string
+	LastName  string
+	// From additional computations
+	PhotoURL *string
+}
+
+func NewUserAlreadyExistsError() *errs.Error {
+	return errs.New(errs.CodeAlreadyExists, "User already exists")
+}
