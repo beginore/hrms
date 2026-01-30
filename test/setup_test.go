3f7f15818d94d6c7a7e1a7c9505a3f64d36b1d03
Shynggys <s.terekbayev@andersenlab.eu>
2026-01-30 15:01:44 +0500

    added sample testing for user and setup

---
+++ test/setup_test.go
@@ -0,0 +1,10 @@
+package test
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	fmt.Println("The main function to prepare and cleanup everything that is needed to different integration tests")
+}
---
+++ test/user_test.go
@@ -0,0 +1,7 @@
+package test
+
+import (
+	"testing"
+)
+
+func TestUser(t *testing.T) {}
